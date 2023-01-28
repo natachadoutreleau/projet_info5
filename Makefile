00e769d55e4f77559fd43c8547b690eea27c549f
Natacha Doutreleau <natacha.doutreleau@gmail.com>
2023-01-28 18:44:21 +0100

    ajout des tests

--- Makefile
+++ Makefile
@@ -16,4 +16,4 @@ String.o: String.cpp String.h
 	g++ -std=c++11 -o String.o -c String.cpp
 
 clean:
-	rm make_lab $(objects)
+	rm bin $(objects)
--- string_tests.cpp
+++ string_tests.cpp
@@ -4,8 +4,20 @@
 
 
 
-int test_default_constructor(){
+int getter_string(){
   String s= new String();
   std::cout<<s.string_<<std::endl;
+  return 0;
+}
+
+int getter_s(){
+  String s= new String();
+  std::cout<<s.s<<std::endl;
+  return 0;
+}
+
+int getter_max_s(){
+  String s= new String();
+  std::cout<<s.max_s<<std::endl;
   return 0;
 }
