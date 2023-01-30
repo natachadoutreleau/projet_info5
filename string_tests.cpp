1ea4194dbfe9a3e7e8e3b5ef663727a020f85959
Clarisse Buton <clarisse.buton@gmail.com>
2023-01-30 20:46:55 +0100

    tests

--- string_tests.cpp
+++ string_tests.cpp
@@ -3,9 +3,6 @@
 #include "String.h"
 #include<iostream>
 
-
-
-
 void test_ctor_c_str(){
     String s;
     s.stingify();
@@ -13,18 +10,18 @@ void test_ctor_c_str(){
 
 
 void test_destructor(){
-  String *s= new String; //sans delete on a une mémoire utilisé a 24 bytes definiteky lost et 15 bytes indirectly lost
-  delete s; // avec le delete on ne perds plus de mémoire
+  String *s= new String; //sans delete on a une mémoire utilisée à 24 bytes definitivement perdue et 15 bytes indirectement perdue
+  delete s; // avec le delete on ne perd plus de mémoire
 }
 
 void test_capacity(){
   String s;
   int capacity=s.capacity();
   if (capacity==15){
-      std::cout  << "La capacité est bien :" << capacity<< std::endl;
+      std::cout  << "The capacity is :" << capacity<< std::endl;
   }
   else{
-      std::cout  << "Mauvaise capacité :" << capacity<< std::endl;
+      std::cout  << "Bad capacity :" << capacity<< std::endl;
   }
 }
 
@@ -32,21 +29,21 @@ void test_reserve_normal(){
   String s;
   s.reserve(20);
   s.stingify();
-  std::cout  << "La nouvelle capacité est :" << s.capacity()<< std::endl;
+  std::cout  << "The capacity is :" << s.capacity()<< std::endl;
 }
 
 void test_reserve_more_than_capacity(){
   String s;
   s.reserve(150);
   s.stingify();
-  std::cout  << "La capicité demandé est 150. La nouvelle capacité est :" << s.capacity()<< std::endl;
+  std::cout  << " The capacity requested is 150. The new capacity is :" << s.capacity()<< std::endl;
 }
 
 void test_reserve_lower_than_size(){
   String s;
   s.reserve(10);
   s.stingify();
-  std::cout  << "La capicité demandé est 10. La nouvelle capacité est :" << s.capacity()<< std::endl;
+  std::cout  << "The capacity requested is 10. The new capacity is :" << s.capacity()<< std::endl;
 }
 
 void test_empty_when_empty(){
@@ -72,14 +69,14 @@ void test_empty_when_not_empty(){
 void test_operator_equal_char(){
   String s("Hello World");
   s.stingify();
-  s="Nouveau string";
+  s="New string";
   s.stingify();
 }
 
 void test_operator_plus_two_str(){
   String s("Hello World");
   s.stingify();
-  s="Nouveau string";
+  s="New string";
   s.stingify();
     std::cout<<s.max_size()<<std::endl;
 }
@@ -103,32 +100,33 @@ void test_cstr(){
 
 
 void test_ctor_cstring(){
-    char var[] = "Je suis un gamin et je suis nul en anglais";
+    char var[] = "I like to code in C++";
     std::cout << var << std::endl;
-    std::cout << "taille : " << sizeof(var) << std::endl;
+    std::cout << "size : " << sizeof(var) << std::endl;
     String s1(var);
-    String a("lalalala");
+    String a("even more than in Python!");
     s1.stingify();
     a.stingify();
 }
 
 void test_length(){
     String s2;
-    std::cout << s2.length() << std::endl;
-    std::cout << s2.size() << std::endl;
+    std::cout << "The size of the String with the length function is " << s2.length() << std::endl;
+    std::cout << "It can also be calculated with the size function and is "<< s2.size() << std::endl;
+    std::cout << "It is incredible that two functions can code the same thing!"<< std::endl;
 }
 
 
 
 void test_resize(){
-    String s("I like to code in C");
+    String s("I like to code in C++");
     s.resize(2,'+');
     s.stingify();
 }
 
 void test_operator_eg(){
     String s;
-    String s1("I like to code in C");
+    String s1("I like to code in C++");
     s = s1;
     s.stingify();
 }
@@ -136,18 +134,19 @@ void test_operator_eg(){
 
 void test_clear(){
     String s;
+    std::cout<<"Before clearing: "<<std::endl;
     s.stingify();
     s.clear();
+    std::cout<<"After clearing: "<<std::endl;
     s.stingify();
-    std::cout << s.length() << std::endl;
+    std::cout << "After clearing, the size is: " << s.length() << std::endl;
 }
 
 void test_operator_equal_c(){
   String s;
   s.stingify();
-  s='a';
+  s='c';
   s.stingify();
-
 }
 
 void test_operator_plus_char(){
